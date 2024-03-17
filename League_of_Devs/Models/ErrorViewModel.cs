3a54edfc47d19db4d52c0c027dda91a8c8f6b82f
M0hammedK <mmm2016alkaf@gmail.com>
2024-03-17 09:33:57 +0300

    Add project files.

---
+++ League_of_Devs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace League_of_Devs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
