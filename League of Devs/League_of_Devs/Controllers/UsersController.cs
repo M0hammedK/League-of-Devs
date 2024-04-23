12c50b7b8dfdd895602e4ad3dd1ffc4b649af99b
Hasan <imrshadow.fx@gmail.com>
2024-04-23 22:19:10 +0300

    new page-users

---
+++ League of Devs/League_of_Devs/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace League_of_Devs.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
